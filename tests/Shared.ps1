c88528397f1ee1c80b6054199eb22d10d8656e93
Trent Willingham <TrentWillingham@gmail.com>
2016-11-11 11:49:59 -0500

    update build and deploy scripts

--- tests/Shared.ps1
+++ tests/Shared.ps1
@@ -1,11 +1,9 @@
 # Dot source this script in any Pester test script that requires the module to be imported.
-
-$ModuleManifestName = 'PoshZabbixTools.psd1'
-$ModuleManifestPath = "$PSScriptRoot\..\src\$ModuleManifestName"
+Set-StrictMode -Version latest
 
 if (!$SuppressImportModule) {
     # -Scope Global is needed when running tests from inside of psake, otherwise
     # the module's functions cannot be found in the PoshZabbixTools\ namespace
-    Import-Module $ModuleManifestPath -Scope Global
+    Import-Module $env:BHPSModuleManifest -Scope Global
 }
 
